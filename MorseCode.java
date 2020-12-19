d2958469e7c4751cf59d23b8fb1f1a15ffa87a48
SobiaF <25015478+SobiaF@users.noreply.github.com>
2020-12-19 12:54:11 +0000

    Create MorseCode.java

---
+++ MorseCode.java
@@ -0,0 +1,14 @@
+// Task: Find the index where the substring . .- - (which means “eat”) begins!
+
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    // Write the code:
+   System.out.println(code.indexOf(". .- -")); 
+    
+  }
+  
+}
